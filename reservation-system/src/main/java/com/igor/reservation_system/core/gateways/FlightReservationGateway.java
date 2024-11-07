e97cc6255014deec1633f3ffed946f120124dde3
Pichau <igorof06@outlook.com>
2024-11-07 12:58:51 -0300

    feat: added flight reservation gateway

---
+++ reservation-system/src/main/java/com/igor/reservation_system/core/gateways/FlightReservationGateway.java
@@ -0,0 +1,9 @@
+package com.igor.reservation_system.core.gateways;
+
+import com.igor.reservation_system.core.entities.FlightReservation;
+
+public interface FlightReservationGateway {
+
+    void makeFlightReservation(FlightReservation flightReservation);
+
+}
